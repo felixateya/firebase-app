87d64e19e8d8a93bc4847900933ae51b4f28d8b8
felixateya <122964383+felixateya@users.noreply.github.com>
2023-11-08 15:49:11 +0300

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -54,7 +54,7 @@ function Register() {
         })
         .catch((err) => {
           setError(err.message);
-          console.log(err.message
+          console.log(err.message)
           setIsLoading(false)
           // ..
         });
