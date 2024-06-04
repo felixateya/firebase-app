4f5b069b4d8b4e84cf09162234613fa19294fc2e
felixateya <mulaafelix6@gmail.com>
2024-06-04 15:43:43 +0300

    fixed overflow in auth pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import { useResolvedPath } from "react-router";
 import { FaRegBell } from "react-icons/fa";
-import {Spinner} from 'react-bootstrap'
+import { Spinner } from "react-bootstrap";
 
 function Header({ user, profile }) {
   const params = useResolvedPath();
@@ -8,17 +8,23 @@ function Header({ user, profile }) {
   return (
     <header className="header">
       <h4>
-        {params.pathname  === '/' ? 'dashboard' : params.pathname.replace("/", "")}
+        {params.pathname === "/"
+          ? "dashboard"
+          : params.pathname.replace("/", "")}
       </h4>
       <div className="all">
         {/* <div className="search">
           <input type="search" placeholder="Search..." name="search" />
           <FaSearch />
         </div> */}
-        
+
         <FaRegBell />
         <div className="profile">
-{profile ? <img className="img" src={profile} alt='profile-pic'/> : <Spinner animation="border" />}
+          {profile ? (
+            <img className="img" src={profile} alt="profile-pic" /> 
+          ) : (
+            <Spinner animation="border" />
+          )}
           <p>{user}</p>
         </div>
       </div>
